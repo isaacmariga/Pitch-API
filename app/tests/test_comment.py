85fe6d2abb2f38381b8f6629f90c876bb04b8710
isaac mariga <isaacwangombe5@gmail.com>
2022-05-13 09:49:30 +0300

    Tests

--- app/tests/test_comment.py
+++ app/tests/test_comment.py
@@ -5,7 +5,7 @@ class TestComment(unittest.TestCase):
     def setUp(self):
         self.group_pick_up = Group( name="Pick-up lines" )
         self.user_jane = User(username = "Jane", password = "banana", email = "jane@doe.com" )
-        self.new_line = Line( line_content="I am Groot", group = self.group_pick_up, user = self.user_jane )
+        self.new_line = Pitch( line_content="I am Groot", group = self.group_pick_up, user = self.user_jane )
         self.new_comment = Comment(comment_content="You need more practice")
 
     def test_instance(self):
